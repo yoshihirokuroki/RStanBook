892ff499fd9f225addf55479a9e883b9928c525d
yoshihirokuroki <yoshihiro.kuroki@gmail.com>
2025-02-02 03:38:45 +0900

    codes are re-written to use other packages to draw diagnostic plots

--- chap04/rstan-save-diagnostics.R
+++ chap04/rstan-save-diagnostics.R
@@ -1,18 +1,35 @@
-library(rstan)
+# library(rstan)
+#
+# load('output/result-model4-5.RData')
+#
+# write.table(data.frame(summary(fit)$summary, check.names=FALSE),
+#   file='output/fit-summary.csv', sep=',', quote=TRUE, col.names=NA)
+#
+#
+# library(ggmcmc)
+# ggmcmc(ggs(fit, inc_warmup=TRUE, stan_include_auxiliar=TRUE),
+#   file='output/fit-traceplot.pdf', plot='traceplot')
+# ggmcmc(ggs(fit), file='output/fit-ggmcmc.pdf')
+#
+#
+# library(coda)
+# pdf(file='output/fit-traceplot-coda.pdf')
+# plot(As.mcmc.list(fit))
+# dev.off()
 
-load('output/result-model4-5.RData')
-
-write.table(data.frame(summary(fit)$summary, check.names=FALSE),
-  file='output/fit-summary.csv', sep=',', quote=TRUE, col.names=NA)
-
-
-library(ggmcmc)
-ggmcmc(ggs(fit, inc_warmup=TRUE, stan_include_auxiliar=TRUE),
-  file='output/fit-traceplot.pdf', plot='traceplot')
-ggmcmc(ggs(fit), file='output/fit-ggmcmc.pdf')
-
-
+library(cmdstanr)
+library(bayesplot)
+library(posterior)
 library(coda)
-pdf(file='output/fit-traceplot-coda.pdf')
-plot(As.mcmc.list(fit))
-dev.off()
+library(ggplot2)
+
+# fitオブジェクトをrdsファイルから読み込む
+fit <- readRDS("chap04/output/fit-model4-5.rds")
+
+# サマリーの出力
+summary_fit <- fit$summary()
+write.table(summary_fit, file = "chap04/output/fit-summary.csv", sep = ",", quote = TRUE, col.names = NA)
+
+traceplot_gg <- mcmc_trace(fit$draws())
+print(traceplot_gg)
+ggsave("chap04/output/fit-traceplot-bayesplot.png", plot = traceplot_gg)
--- chap04/run-model4-5.R
+++ chap04/run-model4-5.R
@@ -1,7 +1,41 @@
-library(rstan)
+# library(rstan)
+#
+# d <- read.csv(file = "chap04/input/data-salary.txt")
+# data <- list(N = nrow(d), X = d$X, Y = d$Y)
+# fit <- stan(file = "chap04/model/model4-5.stan", data = data, seed = 1234)
+#
+# save.image(file = "chap04/output/result-model4-5.RData")
 
-d <- read.csv(file='input/data-salary.txt')
-data <- list(N=nrow(d), X=d$X, Y=d$Y)
-fit <- stan(file='model/model4-5.stan', data=data, seed=1234)
+# 必要なパッケージをロード
+# remotes パッケージがない場合はインストール
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
 
-save.image(file='output/result-model4-5.RData')
+# GitHub から {cmdstanr} をインストール
+if (!requireNamespace("cmdstanr", quietly = TRUE)) {
+  remotes::install_github("stan-dev/cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+  cmdstanr::install_cmdstan(cores = 4, overwrite = TRUE) # 4コアを使用してビルド（適宜変更）
+  cmdstanr::cmdstan_version()
+}
+
+library(cmdstanr)
+# データの読み込み
+d <- read.csv(file = "chap04/input/data-salary.txt")
+data <- list(N = nrow(d), X = d$X, Y = d$Y)
+
+# Stanモデルのコンパイル
+model <- cmdstan_model("chap04/model/model4-5.stan")
+
+# MCMCサンプリングの実行
+fit <- model$sample(
+  data = data,
+  seed = 1234,
+  chains = 4,
+  parallel_chains = 4,
+  iter_warmup = 1000,
+  iter_sampling = 2000
+)
+
+# fitオブジェクトをrdsファイルとして保存
+saveRDS(fit, file = "chap04/output/fit-model4-5.rds")
